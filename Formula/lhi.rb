068f7a4a9ca2316e4f981f7650a2ae8a81bd8621
dnatag <dnatag@gmail.com>
2026-04-01 21:42:22 -0400

    update the fomula

--- Formula/lhi.rb
+++ Formula/lhi.rb
@@ -16,10 +16,10 @@ class Lhi < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/dnatag/lhi/releases/download/v#{version}/lhi-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/dnatag/lhi/releases/download/v#{version}/lhi-aarch64-unknown-linux-musl.tar.g"
       sha256 "fab677b315922b1e1058ef4ab5716b60ee91c85e586969520da76f79e512fe47"
     else
-      url "https://github.com/dnatag/lhi/releases/download/v#{version}/lhi-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/dnatag/lhi/releases/download/v#{version}/lhi-x86_64-unknown-linux-musl.tar.gzz"
       sha256 "ac03fddad5d5e7e513088cc48dbb34114ef54f4dddc59e33e5b06e7fc773474d"
     end
   end
